c3df100296fad074fcba525a80aa22506231f5de
rlegler2012 <rlegler2012@hotmail.com>
2023-03-25 15:02:19 -0700

    has the ability to edit existing pokeman

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const pokemon = require('./models/Pokemon');
 const app = express();
 const port = 4000;
-const pokemons = require ('./models/Pokemon');
+const methodOverride = require("method-override")
 app.get('/', function(req, res) {
     res.send('<h1>Hello World!</h1>');
   });
 //middleware
 app.set ('view engine', 'ejs');
 app.use(express.urlencoded({ extended:false }));
+app.use(methodOverride('_method'))
+
 
 
 app.get('/pokemon/new', (req,res) => {
@@ -16,22 +18,34 @@ app.get('/pokemon/new', (req,res) => {
 })
 
 
-app.get('/pokemon/:id', (req,res) => {
-    const pokemons = pokemon[req.params.id];
-    res.render("show.ejs", {pokemons});
-});
 
 app.get('/pokemon', (req,res) => {
     res.render('index.ejs', {pokemon});
 });
 
 app.post('/pokemon', (req,res) => {
-    let newPokemon = req.body;
+    const type = req.body.type.split(',').map(t => t.trim());
+    const newPokemon = {...req.body, type: type}
     pokemon.unshift(newPokemon);
-    res.redirect('/pokemon')
+    res.redirect('/pokemon');
+  });
+
+app.get('/pokemon/edit/:id', (req, res) => {
+    const pokemonToBeEdited = pokemon[req.params.id];
+    res.render('edit.ejs', {pokemonToBeEdited: pokemonToBeEdited, idx: req.params.id});
 });
 
+app.get('/pokemon/:id', (req,res) => {
+    const pokemons = pokemon[req.params.id];
+    res.render("show.ejs", {pokemons: pokemons, idx: req.params.id});
+});
 
+app.put('/pokemon/edit/:id', (req,res) => {
+    const type = req.body.type.split(',').map(t => t.trim());
+    const editedPokemon = {...req.body, type: type}
+    pokemon[req.params.id] = editedPokemon;
+    res.render('show.ejs', {pokemons: pokemon[req.params.id], idx: req.params.id})
+});
 
 //this go to the end
 app.listen(port, () => {
