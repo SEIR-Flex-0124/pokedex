79cc4f26b1817effc7aa8e7208a58bc3f133e566
uriapete <uriarteangelo@gmail.com>
2023-03-25 10:59:57 -0700

    Got basic express app running, hooked up pokemon model

--- server.ts
+++ server.ts
@@ -1,5 +1,16 @@
+import { Request, Response } from "express";
+
 const express = require("express");
 const app = express();
 
 const PORT:number=4000;
 
+const models = require("./models")
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send(models)
+})
+
+app.listen(PORT,()=>{
+    console.log(`Now listening at PORT ${PORT}...`)
+})
