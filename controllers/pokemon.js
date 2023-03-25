c1d3c3ee51b4ea10ed7e859a5733193f4a85474b
Vbeni <benivillafana@gmail.com>
2023-03-25 14:51:13 -0400

    controllers

---
+++ controllers/pokemon.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const pokemon = require('../models/pokemon');
+
+router.get('/pokemon', (req, res) =>{
+    res.render('pokemon/index.ejs', {pokemon: pokemon})
+});
+
+router.get('/pokemon/new', (req,res) =>{
+    res.render("pokemon/new.ejs")
+})
+
+router.get('/pokemon/:id', (req,res) =>{
+    const pokeman = pokemon[req.params.id];
+    res.render("pokemon/show.ejs", {pokeman: pokeman});
+})
+
+router.post('/pokemon', (req, res) =>{
+    let newPokemon = req.body;
+    pokemon.push(newPokemon);
+    res.redirect('/pokemon');
+})
+
+module.exports = router; 
