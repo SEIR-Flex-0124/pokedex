41d1a81038ecc357119d3d84c5226a219f2de5c1
uriapete <uriarteangelo@gmail.com>
2023-03-27 17:25:51 -0700

    Set up pokemon index page with multiple pages

--- controllers/pokemon.ts
+++ controllers/pokemon.ts
@@ -6,7 +6,10 @@ const router = express.Router();
 const {pokemon} = require("../models");
 
 router.get("/",(req:Request,res:Response)=>{
-    res.render("pokemon/index", {pokemon})
+    let page;
+    if (typeof req.query.page === "undefined"){page = 0}
+    else {page = parseInt(req.query.page!.toString())}
+    res.render("pokemon/index", {pokemon, page})
 })
 
 module.exports=router;
