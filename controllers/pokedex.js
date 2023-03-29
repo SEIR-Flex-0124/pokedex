3e92dfb4acab9f3747c3a606ca477b985d4cef16
fxhanloniii <hanlonfrankiex@gmail.com>
2023-03-29 14:03:02 -0700

    edits to routes, and changed myTeam show page to edit page to make more fluid

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -18,8 +18,21 @@ router.get('/pokemon', (req, res) => {
 router.get('/pokemon/:id', (req, res) => {
     const id = req.params.id;
     const selectedPokemon = pokemon.find(p => p.id === id);
-    res.render('show.ejs', { selectedPokemon, myTeam} );
+    res.render('show.ejs', { selectedPokemon, myTeam, pokemon} );
     console.log(selectedPokemon)
+});
+
+router.get('/pokemon/:id/edit', (req, res) => {
+    const id = req.params.id;
+    const selectedPokemon = pokemon.find(p => p.id === id);
+    res.render('edit.ejs', { myTeam: myTeam, selectedPokemon: selectedPokemon, pokemon: pokemon });
+});
+
+router.post('/pokemon/:id/edit', (req, res) => {
+    const id = req.params.id;
+    const selectedPokemon = pokemon.find(p => p.id === id);
+    selectedPokemon.name = req.body.name;
+    res.redirect('/pokemon');
 });
 
 router.get('/myteam', (req, res) => {
@@ -34,18 +47,9 @@ router.post('/myteam', (req, res) => {
         res.render('myTeam.ejs', { myTeam: myTeam });
     };
 
-router.get('/myteam/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedPokemon = myTeam.find(p => p.id === id);
-    res.render('myTeamShow.ejs', { myTeam: myTeam, selectedPokemon: selectedPokemon });
-});
 
-router.post('/myteam/:id/edit', (req, res) => {
-    const id = req.params.id;
-    const selectedPokemon = myTeam.find(p => p.id === id);
-    selectedPokemon.name = req.body.name;
-    res.redirect('/myteam');
-});
+
+
 
 });
 module.exports = router;
