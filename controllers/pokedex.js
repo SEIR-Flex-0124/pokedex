8e154c6f5922a8691018a9f4b87af1fb469dde71
fxhanloniii <hanlonfrankiex@gmail.com>
2023-03-29 18:25:05 -0700

    added delete and new pages and routes

--- controllers/pokedex.js
+++ controllers/pokedex.js
@@ -7,6 +7,10 @@ router.get('/', (req, res) => {
     res.render('index.ejs', { pokemon: pokemon });
 });
 
+router.get('/new', (req, res) => {
+    res.render("new.ejs")
+})
+
 router.get('/home', (req, res) => {
     res.render('home.ejs', {pokemon: pokemon });
 });
@@ -15,11 +19,38 @@ router.get('/pokemon', (req, res) => {
     res.render('pokemon.ejs', {pokemon: pokemon });
 });
 
+router.post('/new', (req, res) => {
+    const newPokemon = {
+        id: `${pokemon.length + 1 }`,
+        name: req.body.name,
+        img: req.body.img,
+        type: [req.body.type],
+        stats: {
+            hp: 50,
+            attack: 50,
+            defense: 50,
+            spattack: 50,
+            spdefense: 50,
+            speed: 50,
+          },
+        misc: {
+            classification: "misc",
+            height: "4",
+            weight: "89",
+        }
+    };
+    console.log(newPokemon);
+    pokemon.push(newPokemon);
+    
+    res.redirect('/pokemon');
+});
+
 router.get('/pokemon/:id', (req, res) => {
     const id = req.params.id;
+    console.log(id)
     const selectedPokemon = pokemon.find(p => p.id === id);
+    console.log(pokemon[151]);
     res.render('show.ejs', { selectedPokemon, myTeam, pokemon} );
-    console.log(selectedPokemon)
 });
 
 router.get('/pokemon/:id/edit', (req, res) => {
@@ -28,7 +59,7 @@ router.get('/pokemon/:id/edit', (req, res) => {
     res.render('edit.ejs', { myTeam: myTeam, selectedPokemon: selectedPokemon, pokemon: pokemon });
 });
 
-router.post('/pokemon/:id/edit', (req, res) => {
+router.put('/pokemon/:id/edit', (req, res) => {
     const id = req.params.id;
     const selectedPokemon = pokemon.find(p => p.id === id);
     selectedPokemon.name = req.body.name;
@@ -46,10 +77,12 @@ router.post('/myteam', (req, res) => {
         myTeam.push(selectedPokemon);
         res.render('myTeam.ejs', { myTeam: myTeam });
     };
-
-
-
-
-
 });
+
+router.delete('/myteam/:id', (req,res) => {
+    const id = req.params.id;
+    const index = myTeam.findIndex(p => p.id === id);
+    myTeam.splice(index, 1);
+    res.redirect('/myteam');
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+const methodOverride = require('method-override');
 
 const pokedexController = require('./controllers/pokedex');
 
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended:false }));
+
+app.use(methodOverride('_method'));
 
 app.use(express.static('public'));
 
@@ -16,4 +19,4 @@ app.listen(port, () => {
     console.log(`Gotta Catch Em' All On Port ${port}`);
 });
   
-
+  
