651e5bfcdba0e5d8ad72c2548363906fa25ad891
uriapete <uriarteangelo@gmail.com>
2023-03-31 16:09:38 -0700

    Added new post route and changed url to be based on set id property instead of array property

--- controllers/pokemon.ts
+++ controllers/pokemon.ts
@@ -1,5 +1,6 @@
 import exp from "constants";
 import { Request,Response } from "express";
+import { stat } from "fs";
 
 const express = require("express");
 const router = express.Router();
@@ -25,7 +26,36 @@ router.get("/new",(req:Request,res:Response)=>{
 })
 
 router.get("/:id",(req:Request,res:Response)=>{
-    res.render("pokemon/show", {mon:pokemon[parseInt(req.params.id)]})
+    const id:string = req.params.id;
+    let mon:any;
+    for (let i = 0; i < pokemon.length; i++) {
+        const element = pokemon[i];
+        if(element.id===id){
+            mon=element;
+            i=pokemon.length;
+        }
+    }
+    res.render("pokemon/show", {mon})
+})
+
+router.post("",(req:Request,res:Response)=>{
+    const newMon = req.body;
+    newMon.type = [newMon.type1];
+    if(typeof newMon.type2 !== "undefined"){newMon.type.push(newMon.type2)}
+    // const testarr:Array<any> = [];
+    // testarr.push
+    const stats: Object = {
+        hp: newMon.hp,
+        attack:newMon.attack,
+        defense:newMon.defense,
+        spattack:newMon.spattack,
+        spdefense:newMon.spdefense,
+        speed:newMon.speed,
+    };
+    newMon.stats=stats;
+    console.log(newMon);
+    pokemon.push(newMon);
+    res.redirect(`/pokemon/${newMon.id}`);
 })
 
 module.exports=router;
