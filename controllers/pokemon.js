77be23cac9a58b77ca5660c20294c191be51cab4
Signal-flow12 <tcravenny@gmail.com>
2023-04-01 09:23:54 -0400

    Added out put route for edit

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -13,8 +13,46 @@ router.get("/pokemon/new", (req, res) =>{
 router.get("/pokemon/:id", (req, res) => {
     const individualPokemon = pokemon[req.params.id];
     //console.log(individualPokemon)
-    res.render("show.ejs", ({individualPokemon}))
+    res.render("show.ejs", ({individualPokemon, idx: req.params.id}))
 })
+
+router.post("/pokemon", (req, res) => {
+    let newPokemon = req.body
+    //console.log(newPokemon)
+    pokemon.push(newPokemon)
+    //console.log(req.body)
+    res.redirect("/pokemon")
+})
+
+
+router.get("/pokemon/:id/edit", (req, res) => {
+    const editPokemon = pokemon[req.params.id];
+    console.log(editPokemon);
+    res.render("./edit.ejs", {editPokemon, idx: req.params.id});
+})
+
+router.put("/pokemon/:id", (req, res) => {
+    let updatedPokemon = req.body;
+    console.log(updatedPokemon);
+    pokemon[req.params.id] = updatedPokemon
+    res.redirect("/pokemon")
+
+})
+
+// router.put('/:id', (req, res) => {
+//     let updatedFruit = req.body;
+//     if(!updatedFruit.color) updatedFruit.color = "Blue"
+//     if(updatedFruit.readyToEat === 'true') updatedFruit.readyToEat = true;
+//     else updatedFruit.readyToEat = false;
+//     console.log(updatedFruit);
+//     fruits[req.params.id] = updatedFruit;
+//     res.redirect(`/fruits/${req.params.id}`);
+// })
+
+// router.delete("/pokemon/:id", (req, res) => {
+//     pokemon.splice(req.params.id);
+//     res.redirect("/pokemon");
+// })
 
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
+const methodOverride = require("method-override")
 const pokemonController = require('./controllers/pokemon')
 
 //middleware
@@ -13,12 +14,16 @@ app.set('view engine', 'ejs');
 //This enables form not sure how
 app.use(express.urlencoded({ extended:false }));
 
-app.get('/', (req, res) => {
-    res.send(`<h1> home page </h1>`)
-})
+app.use(methodOverride("_method"));
+
 
 //This is linking the pokemon controller
 app.use('', pokemonController);
+
+
+// app.get('/*', (req, res) => {
+//     res.render("404.ejs")
+// })
 
 
 app.listen(PORT, () => {
