64c324f3f0eab3c35e2edf335bb91df6b652a0d2
armullen <amber.r.mullen@gmail.com>
2023-04-03 13:58:16 -0500

    debugging edit route

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -2,33 +2,53 @@ const express = require('express');
 const router = express.Router();
 let pokemon = require('../models/pokemon');
 
+//index route
 router.get('/', (req, res) => {
     res.render('index.ejs', {pokemon})
 })
 
-router.get('/new', (req, res) => {
-    res.render('new.ejs')
-})
-
-router.post('/', (req, res) => {
-    let newPokemon = req.body;
-    pokemon.push(newPokemon);
-    res.redirect('/');
-})
-
+//show route
 router.get('/:id', (req, res) => {
     const singlePokemon = pokemon[req.params.id];
     res.render('show.ejs', {singlePokemon, idx: req.params.id})
 })
 
-router.get('/:id/edit', (req, res) => {
-    const pokemonToBeEdited = pokemon[req.params.id];
-    res.render('edit.ejs', {pokemonToBeEdited, idx: req.params.id});
+//new route
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
 })
 
+router.post('/', (req, res) => {
+    pokemon.push(req.body);
+    res.redirect('/');
+})
+
+//edit route
+router.get('/:id/edit', (req, res) => {
+    res.render('edit.ejs', {pokemon, idx: req.params.id});
+})
+
+router.put('/:id', (req, res) => {
+    let idx = req.params.id;
+    let updatedPokemon = req.body;
+    pokemon[idx] = updatedPokemon;
+    res.redirect('/')
+})
+
+//delete route
 router.get('/:id/delete', (req, res) => {
     const singlePokemon = pokemon[req.params.id];
     res.render('delete.ejs', {singlePokemon, idx: req.params.id});
+})
+
+router.delete('/:id', async (req, res, next) => {
+        try{
+            pokemon.splice(req.params.id, 1)
+            res.redirect('/pokemon')
+        } catch(err) {
+            console.log(err);
+            next();
+        }
 })
 
 
