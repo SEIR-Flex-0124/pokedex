9e7309ff676475b58efd277fdffbc8a61157c5eb
JacobPaul710 <jacobpaulscott@outlook.com>
2023-04-03 17:53:08 -0700

    Creates functional show page for each Pokemon that renders when the image is clicked.

--- Controllers/pokemon.js
+++ Controllers/pokemon.js
@@ -6,4 +6,9 @@ router.get('/', (req, res) => {
 res.render('index.ejs', {Pokemon})
 })
 
+router.get('/:id', (req, res) => {
+    const pokemonCard = Pokemon[req.params.id]
+    res.render('show.ejs', {pokemonCard})
+})
+
 module.exports = router;
