3c9801fd55e6479a60179e4c3af383cadd36bb61
JacobPaul710 <jacobpaulscott@outlook.com>
2023-04-04 09:46:08 -0700

    Establishes 'new' route with the ability to add a pokemon to the pokedex.

--- Controllers/pokemon.js
+++ Controllers/pokemon.js
@@ -6,9 +6,19 @@ router.get('/', (req, res) => {
 res.render('index.ejs', {Pokemon})
 })
 
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+
 router.get('/:id', (req, res) => {
     const pokemonCard = Pokemon[req.params.id]
     res.render('show.ejs', {pokemonCard})
+})
+
+router.post('/', (req, res) => {
+    Pokemon.push(req.body)
+    res.redirect('/pokemon')
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express();
 const PORT = 4000;
 const pokemonController = require('./Controllers/pokemon');
 
+//middleware
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended:false }));
+
 //controller 
 
 app.use('/pokemon', pokemonController)
 
-//middleware
 
-// app.get('/', (req, res) => {
-//     res.send('Test')
-// })
-
+//listen
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`)
