d0fafb99655f73b6b4503ba7be0a3a2e2f8fc694
nickycraig <nickycraig@gmail.com>
2023-04-07 18:04:53 -0700

    fixed edit route and edit page

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -12,9 +12,9 @@ router.get('/pokemon/new', (req, res) => {
 })
 
 router.get('/pokemon/:id', (req, res) => {
-    let singlePoke = pokemon[req.params.id]
+    const singlePoke = pokemon[req.params.id]
     console.log(singlePoke);
-    res.render('show.ejs', {singlePoke})
+    res.render('show.ejs', {singlePoke: singlePoke, idx: req.params.id})
 })
 
 router.post('/pokemon', (req, res) => {
@@ -37,9 +37,29 @@ router.post('/pokemon', (req, res) => {
     res.redirect('/pokemon');
   })
 
-  router.get('/:id/edit', (req, res) => {
+router.get('/pokemon/:id/edit', (req, res) => {
     const pokemonToBeEdited = pokemon[req.params.id];
-    res.render('pokemon/edit.ejs', {pokemonToBeEdited, idx: req.params.id});
+    res.render('edit.ejs', {pokemonToBeEdited, idx: req.params.id});
+})
+
+router.put('/pokemon/:id', (req, res) => {
+    let updatedPokemon = req.body;
+    console.log(updatedPokemon);
+    pokemon[req.params.id] = updatedPokemon;
+    res.redirect(`/pokemon/${req.params.id}`);
+})
+
+router.get('/pokemon/:id/delete', (req, res) => {
+    const pokemonToBeDeleted = pokemon[req.params.id];
+    console.log(pokemonToBeDeleted);
+    res.render('delete.ejs', {pokemonToBeDeleted, idx: req.params.id});
+})
+
+router.delete('/pokemon/:id', (req, res) => {
+    let deletedPokemon = pokemon[req.params.id];
+    console.log(deletedPokemon);
+    pokemon.splice(req.params.id, 1);
+    res.redirect('/pokemon');
 })
 
 module.exports = router;
